994ed17370f8b47219d22f02f2517ac02f513043
Mathencence <Mathencence@users.noreply.github.com>
2022-10-29 06:18:09 +0100

    Prototype-Map done
    Add two transformation function in map generator.

--- Assets/Scripts/GameCore/GameState.cs
+++ Assets/Scripts/GameCore/GameState.cs
@@ -12,25 +12,32 @@ using static MapInstructionEnums.mapInstructionType;
 static class Constants
 {
     //public const double Pi = 3.14159;
-    public const int MAX_MAPSIZE = 129; //Make sure this is an odd numbers, so making sure it has a center. 
-    public const int origin = (MAX_MAPSIZE - 1) / 2;
+    public const int MAX_MAPSIZE = 129; //Make sure this is an odd numbers, so making sure it has a center.
+    public const int origin = 0;//(MAX_MAPSIZE - 1) / 2;
+    public const int MAX_LAYERSIZE = 3;// Can be edited, different game object on the same grid will be stored in different layer.
+
+    
 }
 
 namespace MapEnums {
-public enum mapMaterial{
-    BASE,
-    SPAWNER,
-    ROAD,
-    CUBE, 
-};
-public enum direction
-{
-    EAST,
-    WEST,
-    NORTH,
-    SOUTH,
-}
-
+    public enum mapMaterial{
+        BASE,
+        SPAWNER,
+        ROAD,
+        CUBE, 
+    };
+    public enum direction
+    {
+        EAST,
+        WEST,
+        NORTH,
+        SOUTH,
+    }
+    public enum layer
+    {
+        TERRAIN,
+        CONSTRUCTION,
+    }
 }
 public class GameState : MonoBehaviour
 {
@@ -39,14 +46,24 @@ public class GameState : MonoBehaviour
     public bool isPrototype = false;
 
     // Start is called before the first frame update
-    private int[,] gameGrid = new int[MAX_MAPSIZE, MAX_MAPSIZE];
+    private int[,,] gameGrid = new int[MAX_MAPSIZE, MAX_MAPSIZE, MAX_LAYERSIZE]; //Storage of map information
 
+    private void setGameGrid(int x, int y, layer l, mapMaterial material)
+    {
+        gameGrid[x+ (MAX_MAPSIZE - 1) / 2, y+ (MAX_MAPSIZE - 1) / 2, (int)l] = (int)material;
+        return;
+    }
+    
+    
     private void spawnMapMaterial(mapMaterial material, int x, int y, int height = 0) //Spawn specific material in the location x,y refer to the grid system
     {
-        Vector3 pos; pos.x = origin - x; pos.z = y - origin; pos.y = (0.5f * height);
+        //Will add facing direction in the future, if mapMaterial is differed 
+        
+        Vector3 pos; pos.x = -x; pos.z = -y; pos.y = (0.5f * height);
         Vector3 angle; angle.x = 0.0f; angle.y = 0.0f; angle.z = 0.0f;
 
-        gameGrid[x, y] = (int)material;//Changing the info in grid
+        setGameGrid(x, y, layer.TERRAIN, material); //Storing the info in grid
+
         Instantiate(materialPrefab[(int)material], pos, Quaternion.Euler(new Vector3(0, 0, 0)), MapContainer);
     }
     private void spawnLine(mapMaterial material, Vector2Int start, direction dir, int length, int height = 0) //Use specific material to draw a line from start location x,y to a direction , need to be a straight line
@@ -100,7 +117,8 @@ public class GameState : MonoBehaviour
 
     void Start()
     {
-        gameGrid[origin, origin] = (int)mapMaterial.BASE;
+        setGameGrid(0, 0, layer.TERRAIN, BASE);
+        
         spawnMapMaterial(mapMaterial.BASE, origin, origin);
         if (isPrototype == true)
         {
@@ -109,5 +127,4 @@ public class GameState : MonoBehaviour
         }
         //Debug.Log(gameGrid[origin, origin]);
     }
-
 }
