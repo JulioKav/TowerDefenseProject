408e41bbe7087d41fd183d4d9f767c803f61150b
JulioKav <juliokavaja@hotmail.co.uk>
2022-11-21 16:18:15 +0000

    Wall Turret unpolished

--- Assets/Scripts/Turrets/Spikes/SpikeTurret.cs
+++ Assets/Scripts/Turrets/Spikes/SpikeTurret.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpikeTurret : Mage
+public abstract class SpikeTurret : Mage
 {
     [Header("Turret Stats")]
 
