04b5980a30a429c5918dd91b134b6302814e7e46
JulioKav <juliokavaja@hotmail.co.uk>
2022-12-21 14:37:32 +0000

    merge corrections

--- Assets/Scripts/Enemies/Enemies.cs
+++ Assets/Scripts/Enemies/Enemies.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 
 public class Enemies : MonoBehaviour
 {
+    //Retrieve gamegrid from Map Manager
+    private MapManager mapManager;
+    private Pathfinding pathFinding;
     // Enemy Attributes
     public float speed = 4.0f;
     public bool slowed = false;
@@ -15,6 +18,10 @@ public class Enemies : MonoBehaviour
     public float mech_damage = 50f;
     public GameObject impact_effect;
     public GameObject death_effect;
+
+    //Enemy State
+    public bool isBackward = false;
+    private Vector3 spawner;
 
     // Waypoint
     [HideInInspector]
@@ -41,8 +48,10 @@ public class Enemies : MonoBehaviour
     {
         
         skillManager = GameObject.FindObjectsOfType<SkillManager>()[0];
+        spawner = transform.position;
+        mapManager = FindObjectOfType<MapManager>();
+        pathFinding = FindObjectOfType<Pathfinding>();
 
-        
     }
 
     protected void Update()
@@ -53,21 +62,53 @@ public class Enemies : MonoBehaviour
             StartCoroutine(LowerAfterTime(3, gameObject.transform, speed));
         }
 
-        // Moves enemy to the direction of a waypoint
         if (target == null) return;
-        Vector3 direction = target.position - transform.position;
+        float xdiff = transform.position.x - Mathf.RoundToInt(transform.position.x), zdiff = transform.position.z - Mathf.RoundToInt(transform.position.z);
+        Vector3 direction;
+        if (isBackward != true)
+        {
+            direction = pathFinding.findDirection(mapManager.getLocOnGrid(transform.position), mapManager, new Vector2Int(0, 0));
+        }
+        else
+        {
+            direction = pathFinding.findDirection(mapManager.getLocOnGrid(transform.position), mapManager, mapManager.getLocOnGrid(spawner));
+        }
+
+        // Softly snap the enemy into grid
+        if (direction.x == 0.0f)
+        {
+            direction.x -= 2.5f * xdiff;
+        }
+        else if (direction.z == 0.0f)
+        {
+            direction.z -= 2.5f * zdiff;
+        }
+        //target.position - transform.position;
+        //Debug.Log(direction);
+
         transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
 
+        if (mapManager.getLocOnGrid(transform.position) == new Vector2Int(0, 0))
+        {
+            skillManager.SubtractSkillPoints(20);
+            Destroy(gameObject);
+            return;
+        }
         // If enemy reaches a waypoint, move to next waypoint
+        /*
         if (Vector3.Distance(transform.position, target.position) <= 0.2f)
         {
             GetNextWaypoint();
-        }
+            //Debug.Log(mapManager.getGameGrid(1,1,MapEnums.layer.TERRAIN));
+            //Debug.Log("x:"+transform.localPosition.x+",y:"+transform.localPosition.y+",z:"+transform.localPosition.z);
+            Debug.Log(mapManager.getLocOnGrid(transform.position));
+        }*/
 
-        
-        
+        //Give information of yourself, and the map you're in to pathfind
+        //It will return a direction to go.
 
-        
+
+
     }
     void GetNextWaypoint()
     {
@@ -81,6 +122,7 @@ public class Enemies : MonoBehaviour
         wavepointIndex++;
         target = Waypoints.GetChild(wavepointIndex);
     }
+    
 
 
     //Enemy death/damage taken
@@ -218,16 +260,8 @@ public class Enemies : MonoBehaviour
     }
 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("success1");
-        if (other.gameObject.GetComponent<Tags>().HasTag("Wall") == true)
-        {
-            Debug.Log("success");
-            speed = 0f;
-        }
-    }
-    
+
+
 
 }
 
