89b2f244de74aeb0f72d29d234547bb2cbc29b91
Marius Kilian <mkilian96@gmail.com>
2022-12-31 12:39:14 +0100

    Added music to the relevatn stages of the game

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using System;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
     public static AudioManager instance;
+
+    bool boonSelection = false;
+
+    public float MusicVolume { get { return _musicVolume; } set { _musicVolume = Math.Clamp(value, 0f, 1f); } }
+    private float _musicVolume = 1f;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,7 +26,7 @@ public class AudioManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
             return;
         }
-        
+
 
 
         foreach (Sound s in sounds)
@@ -31,27 +37,94 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
             s.source.outputAudioMixerGroup = s.group;
-            
+
 
 
         }
 
-        
+
     }
 
-    private void Start()
+    void Start()
     {
-        Play("Theme");
+        PlayAfterSeconds("Idle", 3);
+    }
+
+    void OnEnable()
+    {
+        GameStateManager.OnStateChange += GameStateChangeHandler;
+    }
+
+    void OnDisable()
+    {
+        GameStateManager.OnStateChange -= GameStateChangeHandler;
+    }
+
+    void GameStateChangeHandler(GameState newState)
+    {
+        switch (newState)
+        {
+            case GameState.IDLE:
+                PlayAfterSeconds("Idle", 3);
+                break;
+            case GameState.PRE_ROUND:
+                Stop("Idle", GameStateManager.Instance.PreRoundTimeInSeconds + 1);
+                break;
+            case GameState.ROUND_ONGOING:
+                Play("WaveOngoing");
+                break;
+            case GameState.POST_ROUND:
+                Stop("WaveOngoing", GameStateManager.Instance.PostRoundTimeInSeconds);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Stop(string name, float fadeSeconds)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("sound" + name + "not found!");
+            return;
+        }
+        StartCoroutine(FadeSoundInSeconds(s, fadeSeconds));
     }
 
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s==null)
+        if (s == null)
         {
             Debug.LogWarning("sound" + name + "not found!");
             return;
         }
         s.source.Play();
+    }
+
+    IEnumerator FadeSoundInSeconds(Sound s, float seconds)
+    {
+        int decrementsPerSecond = 30;
+        float timeStep = 1.0f / decrementsPerSecond;
+        int steps = (int)(decrementsPerSecond * seconds) + 1;
+        float decrement = s.source.volume / steps;
+        for (int i = 0; i < steps; i++)
+        {
+            s.source.volume -= decrement;
+            yield return new WaitForSeconds(timeStep);
+        }
+        s.source.Stop();
+        s.source.volume = MusicVolume;
+    }
+
+    void PlayAfterSeconds(string name, float seconds)
+    {
+        StartCoroutine(_PlayAfterSeconds(name, seconds));
+    }
+    IEnumerator _PlayAfterSeconds(string name, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Play(name);
     }
 }
