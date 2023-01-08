a74664ad2a702f5b3f37afaf86c6ccc30d13360b
Marius Kilian <mkilian96@gmail.com>
2023-01-08 00:26:58 +0000

    made portals face camera when inactive, and face path when activated

--- Assets/Scripts/Map/PathGenerator.cs
+++ Assets/Scripts/Map/PathGenerator.cs
@@ -26,6 +26,8 @@ public class PathGenerator : MonoBehaviour
 
     Vector3 direction;
 
+    Transform currentSpawnPoint = null;
+
     void GeneratePath(Vector3 _from, Vector3 _to)
     {
         Vector2 from = new Vector2(_from.x, _from.z);
@@ -88,8 +90,13 @@ public class PathGenerator : MonoBehaviour
         while (TilesToDestroy.Count > 0)
         {
             GameObject grass = TilesToDestroy.Dequeue();
+            if (currentSpawnPoint != null)
+            {
+                currentSpawnPoint.GetComponent<PortalManager>().SetPathDirection(grass.transform.position);
+                currentSpawnPoint = null;
+            }
             if (grass == null) continue; // Since same gameobject can enter the queue multiple time
-            GameObject road = Instantiate(roadPrefab, grass.transform.position, Quaternion.identity);
+            GameObject road = Instantiate(roadPrefab, grass.transform.position, Quaternion.identity, transform);
             road.transform.position += 0.5f * Vector3.down;
             Destroy(grass);
             yield return new WaitForSeconds(0.025f);
@@ -129,6 +136,7 @@ public class PathGenerator : MonoBehaviour
     {
         if (inactiveSpawnPoints.Count == 0) return;
         Transform spawnPoint = inactiveSpawnPoints.Dequeue();
+        currentSpawnPoint = spawnPoint;
         if (startingSpawnPoint == null) startingSpawnPoint = spawnPoint;
         spawnPoint.GetComponent<PortalManager>().ActivatePortal();
         spawnPoint.GetComponent<MeshRenderer>().enabled = false;
